aed7c7a1300f7010d04ec8515d36038fc067cdb9
Oguamanam Tony <oguamanamtony@Oguamanams-MacBook-Air.local>
2026-05-31 04:31:24 +0100

    images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -77,7 +77,7 @@ export default function Footer() {
           </div>
 
           <div className={styles.logoCol}>
-            <img src="/images/image.copy.png" alt="Chi Botanical logo" className={styles.logo} />
+            <img src="/images/life.png" alt="Chi Botanical logo" className={styles.logo} />
           </div>
         </div>
 
